eb5574095788b5470e88e3ccf4dd6a119fbe4d6b
김영훈 <hunifive@naver.com>
2022-10-14 11:42:15 +0900

    variable

---
+++ variable.js
@@ -0,0 +1,123 @@
+// Whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+// 선언되지 않은 변수가 있을 때 에러를 띄움
+'use strict';
+// a = 6;
+
+// 2. Variable
+// let (added in ES6)
+
+let globalName = 'global name'; // global scope
+{
+  let name = 'ellie';
+  console.log(name);
+  name = 'hello';
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+  gender = 'male';
+  var gender;
+}
+console.log(gender);
+
+console.log(age);
+age = 4;
+console.log(age);
+var age;
+
+// 3. Constants
+// favor immutable data type always for a few reasons:
+//  - security
+//  - thread safety
+//  - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item: number, string, boolean, null, undefined, symbol
+// object, box container
+// function, first-clss function
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+// number - special numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 431282314798213748912748321748912374914n;
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2);
+// console.log(`value: ${symbol1}, type: ${typeof symbol1}`);
+// description을 사용해 string으로 변환 후 출력
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1.description}`);
+
+// Object, real-life object, data structure
+const ellie = { name: 'ellie', age: 20 };
+console.log(ellie);
+ellie.age = 21;
+console.log(ellie);
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); // h
+console.log(`value: ${text}, type: ${typeof text}`);
+
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
+// run time error, TypeScript를 쓰는 이유
+// console.log(text.charAt(0));
